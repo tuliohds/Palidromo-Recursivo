ffc6a2ded4e60012211599a457005fb95c976faf
Túlio Henrique <tuliohenriques@hotmail.com>
2017-10-08 01:29:29 -0300

    Palindromo Recursivo
    Algoritmo desenvolvido em C# que retorna se uma palavra é palindromo.
    Feito para um exercicio da faculdade.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palidromo
+{
+    class Program
+    {
+        public static bool Palidromo(string palavra, int posicaoInicial, int posicaoFinal)
+        {
+            if (palavra.Length < 3)
+            {
+                return false;
+            }
+            else if (palavra[posicaoInicial] != palavra[posicaoFinal])
+            {
+                return false;
+            }
+            else if (posicaoInicial > posicaoFinal)
+            {
+                return true;
+            }
+            else
+            {
+                return Palidromo(palavra, posicaoInicial + 1, posicaoFinal - 1);
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            string palavra;
+
+            Console.WriteLine("Digite aqui uma palavra: ");
+            palavra = Console.ReadLine().Replace(" ", null);
+
+            bool resultado = Palidromo(palavra, 0, palavra.Length - 1);
+
+            if (resultado == false)
+            {
+                Console.WriteLine("Não é palindromo!");
+            }
+            else
+            {
+                Console.WriteLine("É palindromo!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
